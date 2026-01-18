d1ce1f39c361c040c2984d9f3c1c48b75dae3e05
YiliW01 <yiliwangqq@gmail.com>
2026-01-18 15:59:05 -0800

    finished up to double jump

--- enta-1233-2026-spring-main/Assets/_1233_StudentWork/Scripts/PlayerController.cs
+++ enta-1233-2026-spring-main/Assets/_1233_StudentWork/Scripts/PlayerController.cs
@@ -1,14 +1,92 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 
 public class PlayerController : MonoBehaviour
 {
     private Vector2 _input;
+    private CharacterController _characterController;
+    private Vector3 _direction;
+        
+    [SerializeField] private float smoothTime = 0.05f;
+    private float _currentVelocity;
+
+    [SerializeField] private float speed;
+
+    private float _gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 3.0f;
+    private float _velocity;
+
+    [SerializeField] private float jumpPower;
+    private int _numberOfJumps;
+    [SerializeField] private int maxNumberOfJumps = 2;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        ApplyGravity();
+        ApplyRotation();
+        ApplyMovement();
+               
+    }
+
+    private void ApplyRotation()
+    {
+        if (_input.sqrMagnitude == 0) return;
+
+        var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+    }
+
+    private void ApplyMovement()
+    {
+        _characterController.Move(_direction * speed * Time.deltaTime);
+    }
+
+    private void ApplyGravity()
+    {
+        if (IsGrounded() && _velocity < 0.0f)
+        {
+            _velocity = -1.0f;
+        }
+        else
+        {
+            _velocity += _gravity * gravityMultiplier * Time.deltaTime;
+        }
+        
+        _direction.y = _velocity;
+    }
 
     public void Move(InputAction.CallbackContext context)
     {
         _input = context.ReadValue<Vector2>();
-        Debug.Log(_input);
+        _direction = new Vector3(_input.x, 0.0f, _input.y);
     }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+        if (!IsGrounded() && _numberOfJumps >= maxNumberOfJumps) return;
+        if (_numberOfJumps == 0) StartCoroutine(WaitForLanding());
+
+        _numberOfJumps++;
+        _velocity = jumpPower;
+        //_velocity = jumpPower / _numberOfJumps;
+    }
+
+    private System.Collections.IEnumerator WaitForLanding()
+    {
+        yield return new WaitUntil(() => !IsGrounded());
+        yield return new WaitUntil(IsGrounded);
+
+        _numberOfJumps = 0;
+    }
+
+    private bool IsGrounded() => _characterController.isGrounded;
 }
